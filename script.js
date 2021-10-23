b8f14a5b4a575a599f918b09d7d3bac8f6c150de
Patrick4682 <92613083+Patrick4682@users.noreply.github.com>
2021-10-23 17:22:02 +1100

    hjhj

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let question = {
     title: 'gato',
     alternatives: ['dog', 'cat', 'bird', 'fish'],
     correctAnswer: 1
-  };
+};
   
-  // function for showing the question
-  function showQuestion(q) {
+// function for showing the question
+function showQuestion(q) {
     // 1. select dom element
     let titleDiv = document.getElementById("title");
   
@@ -21,7 +21,7 @@ let question = {
     alts.forEach(function(element, index){
       element.textContent = q.alternatives[index];
     });
-  }
+}
   
-  // call the function
-  showQuestion(question);
+// call the function
+showQuestion(question);
