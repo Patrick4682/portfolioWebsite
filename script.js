08f5eb5a9ce699500b1b807b5515197f8c56dae8
Patrick4682 <92613083+Patrick4682@users.noreply.github.com>
2021-10-23 17:44:32 +1100

    commit

--- script.js
+++ script.js
@@ -3,25 +3,41 @@ let question = {
     title: 'gato',
     alternatives: ['dog', 'cat', 'bird', 'fish'],
     correctAnswer: 1
-};
+  };
   
-// function for showing the question
-function showQuestion(q) {
-    // 1. select dom element
-    let titleDiv = document.getElementById("title");
-  
-    // 2. modify it
+  // function for showing the question
+  function showQuestion(q) {
+    // show question title
+    let titleDiv = document.getElementById('title');
     titleDiv.textContent = q.title;
     
-    // selecting by a query
+    // show alternatives
     let alts = document.querySelectorAll('.alternative');
-    console.log(alts);
     
-    // show each alternative
     alts.forEach(function(element, index){
       element.textContent = q.alternatives[index];
+      
+      // listening for events
+      element.addEventListener('click', function(){
+        // check correct answer
+        if(q.correctAnswer == index) {
+          console.log('Correct Answer!');
+        }
+        else {
+          console.log('Wrong Answer!');
+        }
+      });
+      
     });
-}
+  }
   
-// call the function
-showQuestion(question);
+  // call the function
+  showQuestion(question);
+  
+  // select the button element
+  let btn = document.getElementById('btn');
+  
+  // listening to click events
+  btn.addEventListener('click', function(){
+    console.log('Clicked!');
+  });
